b39eb77467659745197cc04f5f414db002cbffe4
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2021-10-13 21:12:53 +0700

    Add files via upload

---
+++ absensi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
+    <link rel="stylesheet" href="absensi.css">
+    <title>Absensi</title>
+</head>
+<body>
+ <!-- sidebar -->
+    <div class="sidebar">
+        <div class="logo">
+        </div>
+        <button type="button" class="menu"><a href="#"><i class="iconify" data-icon="el:css"></i>Profil</a></button>
+        <button type="button" class="menu"><a href="#"><i class="calendar" data-icon="uil:calendar-alt"></i>Jadwal</a></button>
+        <button type="button" class="currentmenu"><a href="absensi.php">Absensi</a></button>
+        <button type="button" class="menu"><a href="laporan.php">Laporan</a></button>
+        <button type="button" class="menu"><a href="#">Logout</a></button>
+    </div>
+
+    <!-- kontent -->
+    <div class="main">
+        <h2>Absensi</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices lorem viverra, pulvinar elit in, feugiat mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis blandit tellus mauris. Aenean ullamcorper tortor tempus justo fringilla, sed maximus purus fermentum. Donec nec neque quis enim elementum facilisis. Morbi tincidunt mauris eu tortor pulvinar commodo. Nunc erat metus, elementum id luctus et, placerat a ligula. Vivamus non efficitur massa. Sed fringilla dictum turpis, et varius nulla elementum id. Integer eget elit dapibus, placerat libero nec, viverra enim. Nullam sed elit in purus dignissim pretium in tempor dolor. Aenean nec suscipit felis.</p>
+        <p>Donec finibus bibendum orci quis facilisis. Proin sit amet ullamcorper augue. Duis pharetra urna sed tellus suscipit suscipit. Nullam in odio dictum, placerat justo eu, interdum velit. Sed nec mollis magna. Aliquam dolor dui, mattis volutpat tincidunt ac, sodales in erat. Praesent porttitor blandit mi, id euismod tortor pretium vitae. Proin at porttitor ex, id venenatis enim. Aenean pretium massa ex, eu elementum ligula molestie sed.</p>
+        <p>Curabitur commodo mi erat, a finibus nunc sollicitudin ut. Phasellus egestas ipsum non libero scelerisque cursus. Aenean nec accumsan lacus. Fusce mattis accumsan odio nec eleifend. Mauris lobortis leo ut volutpat malesuada. Quisque tempor velit ut molestie maximus. Vestibulum ullamcorper auctor lacinia. Aliquam condimentum vehicula efficitur. Cras auctor tellus felis, sit amet vestibulum diam finibus eu. Curabitur ultricies sollicitudin ultricies. Proin urna mauris, porttitor sit amet feugiat non, gravida at metus. Mauris tempus faucibus purus sit amet tempor. Cras condimentum arcu mauris, eu tempor justo laoreet vel. Nam eget magna ipsum.</p>
+        <p>Mauris dapibus ex eu venenatis porta. Cras tempus ante et lectus ullamcorper condimentum. Quisque finibus accumsan placerat. Nam vel purus ac nunc tincidunt condimentum. Mauris nec purus convallis, convallis augue mollis, dictum augue. Etiam nulla ante, faucibus id purus euismod, vehicula tincidunt ante. Phasellus eu est arcu. Proin elementum sed justo id suscipit. Cras vulputate purus velit, consequat bibendum odio ultrices id.</p>
+        <p>Cras laoreet interdum massa, eget pretium nisl bibendum a. Mauris facilisis hendrerit mi sed volutpat. Vestibulum efficitur sed ipsum nec convallis. Praesent porttitor quam eget molestie tincidunt. Duis interdum quam et posuere dignissim. Ut vestibulum tortor viverra nisl semper finibus gravida ut dolor. Nunc ornare augue eros, ut venenatis nulla placerat eu. Nunc a eros tempus, ultrices elit nec, sagittis mi. Donec scelerisque ullamcorper commodo. Curabitur eget laoreet metus.</p>
+        <p>Mauris dapibus ex eu venenatis porta. Cras tempus ante et lectus ullamcorper condimentum. Quisque finibus accumsan placerat. Nam vel purus ac nunc tincidunt condimentum. Mauris nec purus convallis, convallis augue mollis, dictum augue. Etiam nulla ante, faucibus id purus euismod, vehicula tincidunt ante. Phasellus eu est arcu. Proin elementum sed justo id suscipit. Cras vulputate purus velit, consequat bibendum odio ultrices id.</p>
+    </div>
+</body>
+</html>
+
+
