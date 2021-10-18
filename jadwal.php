07df336de09d9a39e2f7a61393a91b0c57ef0de1
VionaA <74656597+VionaA@users.noreply.github.com>
2021-10-18 20:04:14 +0700

    Create jadwal.php

---
+++ jadwal.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
+    <link rel="stylesheet" href="jadwal.css">
+    <title>Jadwal</title>
+</head>
+<body> 
+  <!-- sidebar -->
+    <div class="sidebar">
+        <div class="logo">
+        </div>
+        <button type="button" class="menu"><a href="#"><i class="profil" data-icon="bx:bxs-user"></i>&ensp;Profil</a></button>
+        <button type="button" class="currentmenu"><a href="jadwal.php"><i class="calendar" data-icon="uil:calendar-alt"></i>&ensp;Jadwal</a></button>
+        <button type="button" class="menu"><a href="#"><i class="absensi" data-icon="ci:edit"></i>&ensp;Absensi</a></button>
+        <button type="button" class="menu"><a href="#"><i class="laporan" data-icon="clarity:clipboard-line"></i>&ensp;Laporan</a></button>
+        <button type="button" class="menu"><a href="#"><i class="exit" data-icon="icomoon-free:exit"></i>&ensp;Keluar</a></button>
+    </div>
+	
+	<!-- kontent -->
+	<div class="main">
+		<div class="ID"> 
+		  <table align="left" border="0" cellspacing="20" width="216px">
+                    <tbody>
+                        <td>ID</td>
+                        <td>:</td>
+                        <td>001</td>
+                    </tbody>
+                </table>
+		</div>
+            
+		<div class="Nama"> 
+		    <table align="left" border="0" cellspacing="20" width="240px">
+                    <tbody>
+                        <td>Posisi</td>
+                        <td>:</td>
+                        <td>Akuntan</td>
+                    </tbody>
+                </table>
+		</div>
+		
+		<div class="Posisi"> 
+		<table align="left" border="0" cellspacing="20" width="216px">
+                    <tbody>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>Eric</td>
+                    </tbody>
+                </table>
+		</div>
+		<div class="Departemen"> 
+		<table align="left" border="0" cellspacing="20" width="240px">
+                    <tbody>
+                        <td>Departemen</td>
+                        <td>:</td>
+                        <td>Keuangan</td>
+                    </tbody>
+                </table>
+		</div>
+		
+		<div class="tabel">
+            <table border="0" width="500px">
+                <tbody>
+                    <tr>
+                        <th width="5px" bgcolor="#e5e5e5">No</th>
+                        <th width="40px" bgcolor="#e5e5e5">Hari</th>
+                        <th width="30px" bgcolor="#e5e5e5">Jam Masuk</th>
+                        <th width="30px" bgcolor="#e5e5e5">Jam Keluar</th>
+						            <th width="60px" bgcolor="#e5e5e5">Shift</th>
+                    </tr>
+                    <tr>
+                        <td bgcolor="white">1</td>
+                        <td bgcolor="white">Senin</td>
+                        <td bgcolor="white">08:00</td>
+                        <td bgcolor="white">17:00</td>
+						            <td bgcolor="white">Pagi</td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="white">2</td>
+                        <td bgcolor="white">Selasa</td>
+                        <td bgcolor="white">08:00</td>
+                        <td bgcolor="white">17:00</td>
+                        <td bgcolor="white">Pagi</td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="white">3</td>
+                        <td bgcolor="white">Rabu</td>
+                        <td bgcolor="white">08:00</td>
+                        <td bgcolor="white">17:00</td>
+                        <td bgcolor="white">Pagi</td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="white">4</td>
+                        <td bgcolor="white">Kamis</td>
+                        <td bgcolor="white">08:00</td>
+                        <td bgcolor="white">17:00</td>
+                        <td bgcolor="white">Pagi</td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="white">5</td>
+                        <td bgcolor="white">Jumat</td>
+                        <td bgcolor="white">08:00</td>
+                        <td bgcolor="white">17:00</td>
+                        <td bgcolor="white">Pagi</td>
+					</tr>
+					<tr>
+                        <td bgcolor="white">6</td>
+                        <td bgcolor="white">Sabtu</td>
+                        <td bgcolor="white">20:00</td>
+                        <td bgcolor="white">06:00</td>
+                        <td bgcolor="white">Malam</td>
+                </tbody>
+            </table>
+        </div>
+</body>
+</html>
